50cafae86559a7df026bfaca7bca295734c5ffe1
Surekha Wanave <surekha.wanave11@gmail.com>
2020-02-10 22:33:23 +0530

    this is my second commit

---
+++ src/Components/Buttons.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react"
+
+class Button extends Component{
+    render(){
+        return(
+            <div className="cals-wrapper">
+                <div className="row">
+                    <button></button>
+                    <button></button>
+                    <button></button>
+                    <button></button>
+                </div>
+                <div className="row">
+                    <button></button>
+                    <button></button>
+                    <button></button>
+                    <button></button>
+                </div>
+                <div className="row">
+                    <button></button>
+                    <button></button>
+                    <button></button>
+                    <button></button>
+                </div>
+
+            </div>
+        )
+    }
+}
